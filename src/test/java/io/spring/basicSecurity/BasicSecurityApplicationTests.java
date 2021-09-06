e0eb4a2eff9ab673508e96f604a43a552e592f81
std-freejia <std-freejia@gmail.com>
2021-09-06 18:24:28 +0900

    Initial commit

---
+++ src/test/java/io/spring/basicSecurity/BasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.basicSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
