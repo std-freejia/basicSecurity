81b6ae3460eee7b10febc721b00cabc210926d55
std-freejia <std-freejia@gmail.com>
2021-09-06 22:07:03 +0900

    [Form Login 인증]
    인증 받지 않은 클라이언트는(요청은) 로그인 페이지로 리다이렉트
    post방식으로 인증 시도하고 성공하면, 서버가 session id를 생성.
    session에 인증 토큰을 생성하고 서버에 저장함.
    security context를 session에 저장함.
    클라이언트가 다시 자원에 접근을 시도하면, 서버는 session에 저장된 인증 토큰으로 접근 가능 여부 확인

--- src/main/java/io/spring/basicSecurity/SecurityConfig.java
+++ src/main/java/io/spring/basicSecurity/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity // 웹 보안 활성화 시킴
@@ -18,7 +27,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         // 인증 - form 기반
         http
-                .formLogin();
+                .formLogin()
+                // .loginPage("/loginPage") // 로그인 폼 페이지 (인증 받지 않은 누구나 접근 가능한 페이지)
+                .defaultSuccessUrl("/") // 인증에 성공했을 때 이동할 url
+                .failureUrl("/login")
+                .usernameParameter("userId") // input 필드명 지정 가능
+                .passwordParameter("passwd") // input 필드명 지정 가능
+                .loginProcessingUrl("/login_proc") // form 태그의 action url 속성 지정
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override // 로그인 성공시,
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        System.out.println("authentication = " + authentication.getName()); // 인증에 성공한 사용자이름
+                        response.sendRedirect("/");
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override // 인증 실패시,
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+                        // 다시 로그인할 수 있는 로그인 페이지로 보냄
+                        System.out.println("e.getMessage() = " + e.getMessage());
+                        response.sendRedirect("/login");
+                    }
+                })
+                .permitAll();
     }
 
 }
